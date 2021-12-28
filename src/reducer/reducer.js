f9ac24f165c45cfed2c8735df0a9ecafa7e7c1be
Jerick Iquin - Home <jci.dev.0@gmail.com>
2021-12-28 12:33:41 -0800

    added reducer function for usereducer

---
+++ src/reducer/reducer.js
@@ -0,0 +1,24 @@
+import { v4 as uuid } from 'uuid';
+
+export const ACTIONS = {
+	ADD_TIMER: 'add_timer',
+	DELETE_TIMER: 'delete_timer'
+};
+
+export function reducer(timers, action) {
+	switch (action.type) {
+		case ACTIONS.ADD_TIMER:
+			return [...timers, newTimer(action.payload)];
+		case ACTIONS.DELETE_TIMER:
+			return timers.filter((timer) => timer.id !== action.payload);
+		default:
+			return timers;
+	}
+}
+
+function newTimer(inputObj) {
+	return {
+		id: uuid(),
+		...inputObj
+	};
+}
