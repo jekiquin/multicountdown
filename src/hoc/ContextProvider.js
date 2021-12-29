bce95605da36a3bf094a545e04618c1c46f22b94
Jerick Iquin - Home <jci.dev.0@gmail.com>
2021-12-29 08:04:05 -0800

    added provider for archive context

--- src/hoc/ContextProvider.js
+++ src/hoc/ContextProvider.js
@@ -2,14 +2,25 @@ import { createContext, useContext, useReducer } from 'react';
 import { reducer } from 'reducer/reducer';
 
 const TimerContext = createContext();
+const ArchiveContext = createContext();
 
 export function useTimers() {
 	return useContext(TimerContext);
 }
 
+export function useArchive() {
+	return useContext(ArchiveContext);
+}
+
 export default function ContextProvider({ children }) {
 	const [timers, dispatch] = useReducer(reducer, []);
+	const [archives, archiveDispatch] = useReducer(reducer, []);
 	const store = { timers, dispatch };
+	const archive = { archives, archiveDispatch };
 
-	return <TimerContext.Provider value={store}>{children}</TimerContext.Provider>;
+	return (
+		<TimerContext.Provider value={store}>
+			<ArchiveContext.Provider value={archive}>{children}</ArchiveContext.Provider>
+		</TimerContext.Provider>
+	);
 }
